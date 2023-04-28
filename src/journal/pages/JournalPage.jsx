bf0e86255232bc2708a232c5401b5478ee0bdce2
DanielVz <danielrvp27@gmail.com>
2023-04-27 23:54:00 -0700

    inicio navbar, layout journalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,14 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quisquam nostrum iure quae sed, modi corrupti animi officia qui fuga, blanditiis odit nihil laboriosam provident! Repellat vero inventore culpa sint?</Typography>
+      
+      {/* NothingSelected */}
+      {/* NoteView */}
+    </JournalLayout>
     
     
   )
