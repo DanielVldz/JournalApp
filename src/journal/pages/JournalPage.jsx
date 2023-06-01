c44af13d892160840e104d2d5de4680da67e0944
DanielVz <danielrvp27@gmail.com>
2023-06-01 00:33:01 -0700

    dispatch de creación de nueva nota en firebase

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,8 +2,17 @@ import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 import { AddOutlined } from "@mui/icons-material"
+import { startNewNote } from "../../store/journal/thunks"
+import { useDispatch } from "react-redux"
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  }
+
   return (
     <JournalLayout>
       {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quisquam nostrum iure quae sed, modi corrupti animi officia qui fuga, blanditiis odit nihil laboriosam provident! Repellat vero inventore culpa sint?</Typography> */}
@@ -12,6 +21,7 @@ export const JournalPage = () => {
       {/* <NoteView /> */}
 
       <IconButton
+      onClick={ onClickNewNote }
         size='large'
         sx={{
           color: 'white',
