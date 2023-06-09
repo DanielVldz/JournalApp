553010de0f1d7a7b2cd2101371c56f2928d90c82
DanielVz <danielrvp27@gmail.com>
2023-06-08 23:28:55 -0700

    actualizar nota local y en firestore + alerta de guardado exitoso

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -26,15 +26,26 @@ export const journalSlice = createSlice({
         },
         setActiveNote: (state, action ) => {
             state.active = action.payload;
+            state.messageSaved = '';
         },
         setNotes: (state, action ) => {
             state.notes = action.payload;
         },
         setSaving: ( state ) => {
-
+            state.isSaving = true;
+            state.messageSaved = '';
+            // TODO: mensaje de error...
         },
         updateNote: ( state, action ) => {
+            state.isSaving = false;
+            state.notes = state.notes.map( note => { // payload: note 
+                if ( note.id === action.payload.id ) {
+                    return action.payload;
+                }
+                return note;
+            } );
 
+            state.messageSaved = `${ action.payload.title }, actualizada correctamente`;
         },
         deleteNotebyId: ( state, action ) => {
 
